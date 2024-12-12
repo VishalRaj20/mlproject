fe5f330a07f30f4c638d97cb23d7cc911e3961fc
VishalRaj20 <126649319+VishalRaj20@users.noreply.github.com>
2024-12-13 01:24:42 +0530

    first commit

---
+++ src/mlproject/utils.py
@@ -0,0 +1,39 @@
+## this utils file is used for reading from mysql
+import os
+import sys
+
+from mlproject.exception import CustomException
+from mlproject.logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+import pymysql 
+
+load_dotenv()
+
+host=os.getenv("host")
+user=os.getenv("user")
+password=os.getenv("password")
+db=os.getenv("db")
+
+def read_sql_data():
+    logging.info("Reading SQL database Started")
+    
+    try:
+        mydb = pymysql.connect(
+            host = host,
+            user=user,
+            password=password,
+            db=db
+        )
+        logging.info("Connection established",mydb)
+        df = pd.read_sql_query("select * from students",mydb)
+        print(df.head())
+        
+        return df
+        
+        
+    except Exception as e:
+        raise CustomException(e)
+        
+    
+    
